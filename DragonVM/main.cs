587398ae83637f54201d449cce2279d3d03db9a4
BuildTools <unconfigured@null.spigotmc.org>
2020-01-20 22:01:17 -0500

    Added Base Classes

---
+++ DragonVM/main.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class MainClass {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Press Enter to Exit!");
+        Console.Read();
+    }
+}
